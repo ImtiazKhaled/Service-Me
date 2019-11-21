34f0966bd305dd01e70c26801279e9672648a719
Imtiaz <imtiazkhaled07@gmail.com>
2019-11-21 03:52:45 -0600

    debugged backend

--- server/routes/appointment.js
+++ server/routes/appointment.js
@@ -1,3 +1,29 @@
+const app = require("express")
+const router  = app.Router()
+const config = require("../connection")
+const connection = config.connection
+
+
+// get all other users messaged by user
+router.get("/:id", (req, res) => {
+    var id = req.params.id
+    const retObj = []
+    const GET_MESSAGES_BETWEEN = `SELECT * FROM MESSAGESBETWEEN, SMUSER WHERE Messagee=UserId AND Messager="${id}"`
+    console.log(GET_MESSAGES_BETWEEN)
+    connection.query(GET_MESSAGES_BETWEEN, (err, data) => {
+        if(data) {
+            if(data.length>0) {
+                res.send(data)
+            } else {
+                res.send({res:"empty"})
+            }
+        } else {
+            console.log(err)
+        }
+    })
+})
+
+
 // add a new order
 router.post("/", (req, res) => {
     var OrderId =  req.body.CustomerFName[0] + req.body.CustomerLName[0] + req.body.VendorId + req.body.ServiceDate.getTime()
@@ -11,3 +37,5 @@ router.post("/", (req, res) => {
     })
     res.send("success")
 })
+
+module.exports = router
