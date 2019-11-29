d20123e5971a82cf0c1c790bb9b1151d0b7fc3a5
Imtiaz <imtiazkhaled07@gmail.com>
2019-11-29 02:54:14 -0600

    login modal on all pages

--- client/components/pleaseLogin.js
+++ client/components/pleaseLogin.js
@@ -1,30 +1,41 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
 import { ScrollView, View, Text } from "react-native"
+import { Overlay } from "react-native-elements"
 import { Button } from "react-native-elements"
 import { styles, SH, SW } from "../styles/styles"
+import Login from './login'
 
-class Profile extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      user: {},
-      orders: []
-    }
-  }
+const PleaseLogin = (props) => {
 
-  render() {
-    return (
-        <View style={styles.container}>
-            <Text style={{fontSize: 40, paddingTop: SH*0.30}}>
-                Please
-            </Text>
-            <Button containerStyle={{width: SW * 0.50}} title="Login" />
-            <Text style={{fontSize: 40}}>
-                to continue
-            </Text>
-        </View>
-    )
-  }
+  const [loginVisible, changeVisibility] = useState(false)
+
+  return <View style={styles.container}>
+      <Text style={{fontSize: 40, paddingTop: SH*0.30}}>
+          Please
+      </Text>
+      <Button 
+        containerStyle={{width: SW * 0.50}} 
+        title="Login" 
+        onPress={() => changeVisibility(true)}
+      />
+      <Text style={{fontSize: 40}}>
+          to continue
+      </Text>
+      <Overlay
+        isVisible={loginVisible}
+        windowBackonBackdropPress={() => changeVisibility(false)}
+        groundColor="rgba(255, 255, 255, .5)"
+        overlayBackgroundColor="white"
+        width={SW*0.7}
+        height={SH*0.5}
+      >
+        <ScrollView>
+          <Login 
+            closeModal={() => changeVisibility(false)}
+          />
+        </ScrollView>
+      </Overlay>    
+  </View>
 }
 
-export default Profile
+export default PleaseLogin
