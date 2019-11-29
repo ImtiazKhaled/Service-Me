9f2ecd50f5177c23b26cff55b9864a63ee3ff919
Imtiaz <imtiazkhaled07@gmail.com>
2019-11-29 02:20:10 -0600

    bug fixes

--- server/routes/customer.js
+++ server/routes/customer.js
@@ -32,10 +32,17 @@ router.post("/", (req, res) => {
     if(rand<10) rand="0"+rand
     var today = mm+""+dd+""+rand
     const UserId = req.body.FName.charAt(0) + req.body.LName.charAt(0) + today
+    var SendAt = new Date().getTime()
+    var OrderId =  req.body.FName + req.body.LName + UserId + SendAt
+    const MessageId = UserId + UserId + SendAt
     
     const ADD_USER = `INSERT INTO SMUSER VALUES("${UserId}","${req.body.FName}","${req.body.LName}","${CreatedAt}","${req.body.ProfilePicture}")`
     const ADD_VENDOR = `INSERT INTO CUSTOMER VALUES("${UserId}","${req.body.Email}","${req.body.Phone}")`
-    console.log(ADD_USER+"\n"+ ADD_VENDOR)
+    const ADD_CHAT = `INSERT INTO MESSAGESBETWEEN VALUES("${UserId}","${UserId}")`
+    const ADD_MESSAGE = `INSERT INTO MESSAGES VALUES("${MessageId}","${UserId}","${UserId}","${"yes"}","${"yes"}")`
+    const ADD_ORDER = `INSERT INTO GENERATEDORDER VALUES("${OrderId}","${req.body.FName}","${req.body.LName}","${req.body.Email}","${req.body.Phone}","${UserId}","${0}","${0.00}","N/A","${SendAt}","N/A","${req.body.FName}",${null})`
+    
+    console.log(ADD_USER+"\n"+ ADD_VENDOR+"\n"+ADD_CHAT+"\n"+ADD_MESSAGE+"\n"+ADD_ORDER)
 
     connection.query(ADD_USER, (err, data) => {
         if(err) {
@@ -51,6 +58,27 @@ router.post("/", (req, res) => {
         }
     })
 
+    connection.query(ADD_CHAT, (err, data) => {
+        if(err) {
+            console.log(err)
+            res.send("error")
+        }
+    })
+
+    connection.query(ADD_MESSAGE, (err, data) => {
+        if(err) {
+            console.log(err)
+            res.send("error")
+        }
+    })
+
+    connection.query(ADD_ORDER, (err, data) => {
+        if(err) {
+            console.log(err)
+            res.send("error")
+        }
+    })
+    
     res.send("success")
     
 })
